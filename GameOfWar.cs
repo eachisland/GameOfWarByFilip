21388e16d16c49b21b9e6fd336c0188d061e6458
eachisland <fil.dragalevzi@gmail.com>
2023-12-06 13:56:32 +0200

    Add project files.

---
+++ GameOfWar.cs
@@ -0,0 +1,10 @@
+﻿namespace GameOfWarByFilip
+{
+    internal class GameOfWar
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
